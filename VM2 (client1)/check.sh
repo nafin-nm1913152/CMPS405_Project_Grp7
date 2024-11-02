02196f8f15bd262d59eed0fb3614e727e242fac6
KhamisAlkubaisi <ka1707893@qu.edu.qa>
2024-11-02 17:47:31 +0300

    Create check.sh

---
+++ VM2 (client1)/check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Locate the files with 777 permissions and save to a temporary log
+find / -type f -perm 777 > permission_log.log
+
+# Display and update permissions for each file found
+while read -r file_entry; do
+    echo "File with 777 permissions: $file_entry"  # Display file with 777 permission
+    chmod 700 "$file_entry"  # Change permissions to 700
+done < permission_log.log
+
+echo "Permission changes complete for files with 777 permissions."
